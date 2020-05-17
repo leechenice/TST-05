e2d654865b90ce11fa0a018950d5c73edfaec99d
leechenice <leechenice@gmail.com>
2020-05-17 18:05:30 +0800

    :>

--- 0515/src/BinaryTree.java
+++ 0515/src/BinaryTree.java
@@ -92,6 +92,76 @@ public class BinaryTree {
         }
         return getSize4(root.right) + getSize4(root.left);
     }
+    // 子问题思路-求第 k 层结点个数
+    int getKLevelSize(TNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+        if (k == 1) {
+            return 1;
+        }
+        return getKLevelSize(root,k-1) + getKLevelSize(root,k-1);
+    }
+    // 获取二叉树的高度
+    int getHeight(TNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+    }
+
+     // 查找 val 所在结点，没有找到返回 null
+    // 按照 根 -> 左子树 -> 右子树的顺序进行查找
+   // 一旦找到，立即返回，不需要继续在其他位置查找
+    TNode find(TNode root, int val) {
+        if(root == null) { return null; }
+        if(root.val == val) { return root; }
+        TNode node =  find(root.left,val);
+        return node == null ? find(root.right,val) : node;
+
+    }
+    public boolean isSubtree(TNode s, TNode t) {
+        if(s == null || t == null) { return false; }
+        if (isSameTree(s,t)){ return true; }
+        if (isSubtree(s.left,t)) { return true; }
+        return isSubtree(s.right, t);
+
+    }
+
+    public boolean isSameTree(TNode p, TNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        if (p.val != q.val)
+            return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    public boolean isBalance(TNode root) {
+        if (root == null) {
+            return true;
+        }
+        return Math.abs(getHeight(root.left)-getHeight(root.right)) < 2
+                && isSymmetric(root.left) && isSymmetric(root.right);
+    }
+    public boolean isSymmetric(TNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetricChild(root.left,root.right);
+    }
+    public boolean isSymmetricChild(TNode treeLeft,TNode treeRight) {
+        if (treeLeft == null && treeRight == null) {
+            return true;
+        }
+        if (treeLeft == null || treeRight == null) {
+            return false;
+        }
+        return treeLeft.val == treeRight.val
+                && isSymmetricChild(treeLeft.left,treeRight.right)
+                && isSymmetricChild(treeLeft.right,treeRight.left);
+    }
+
 }
 
 
